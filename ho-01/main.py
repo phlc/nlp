deac8f8ea61fe6374c715baf6c26064f69a917c9
wladbrandao <>
2023-02-10 00:35:41 +0000

    h0-01 ok

--- ho-01/main.py
+++ ho-01/main.py
@@ -1,2 +1,9 @@
-file = open("../datasets/Shakespeare.txt")
-print(file.read())
+input = open("../datasets/Shakespeare.txt")
+output = open("Shakespeare_normalized.txt", "w+")
+text = input.read()
+text = text.lower()
+for char in text:
+  if (not char.isalnum()):
+    text = text.replace(char, " ")
+
+output.write(text)

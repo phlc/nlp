acc22e756aa30d0254e109ac741fd2a967fceac7
phlc <30514743+phlc@users.noreply.github.com>
2023-02-10 01:14:52 +0000

    comments

--- ho-01/main.py
+++ ho-01/main.py
@@ -1,9 +1,20 @@
+
+#Open Shakespeare file
 input = open("../datasets/Shakespeare.txt")
+
+#Open outpu file 
 output = open("Shakespeare_normalized.txt", "w+")
+
+#Read Shakespeare.txt
 text = input.read()
+
+#Change all characters to lower case
 text = text.lower()
+
+#Replace non alphanumeric characters with one blank space
 for char in text:
   if (not char.isalnum()):
     text = text.replace(char, " ")
 
+#Write normalized text to output file
 output.write(text)

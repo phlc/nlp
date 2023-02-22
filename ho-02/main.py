6f7b26eacc756a1de31d7eabe4cf9893d0eaa4e5
phlc <30514743+phlc@users.noreply.github.com>
2023-02-22 18:54:16 +0000

    Tokenization 1-4 / Normalization needs improvement

---
+++ ho-02/main.py
@@ -0,0 +1,113 @@
+from unidecode import unidecode
+import nltk
+import nltk.tokenize as to
+import regex as re
+
+nltk.download('punkt')
+
+"""
+Inputs
+"""
+
+#Test Text
+test = "It's true, Ms. Martha Töpfer! $3.00 on 3/21/2023 in cash for an ice-cream in the U.S. market? :-( #Truth"
+
+#Shakespeare file
+input = open("../datasets/Shakespeare.txt")
+shakespeare = input.read()
+"""
+Text Normalization
+"""
+
+testNormalized = unidecode(test)
+testNormalized = testNormalized.lower()
+testNormalized = re.sub('[^\w\d\s$/]', '', testNormalized)
+
+
+shakespeareNormalized = unidecode(shakespeare)
+shakespeareNormalized = shakespeareNormalized.lower()
+shakespeareNormalized = re.sub('[^\w\d\s$/]', '', shakespeareNormalized)
+
+print("\nTest String Normalized: " + testNormalized)
+output = open("ShakespeareNormalized.txt", "w+")
+output.write(shakespeareNormalized)
+
+
+"""
+Text Tokenization
+"""
+#White Space Tokenization
+
+testToken01 = testNormalized.split()
+
+shakespeareToken01 = shakespeareNormalized.split()
+
+print("\nTest String 1: ", testToken01)
+output = open("ShakespeareTokenized01.txt", "w+")
+output.write("["+ (", ".join(shakespeareToken01)) +"]")
+
+# #NLTK: Word Tokenizer
+
+testToken02 = to.word_tokenize(testNormalized)
+
+shakespeareToken02 = to.word_tokenize(shakespeareNormalized)
+
+print("\nTest String 2: ", testToken02)
+output = open("ShakespeareTokenized02.txt", "w+")
+output.write("["+ (", ".join(shakespeareToken02)) +"]")
+
+# #NLTK: Tree Bank Tokenizer
+
+testToken03 = to.TreebankWordTokenizer().tokenize(testNormalized)
+
+shakespeareToken03 = to.TreebankWordTokenizer().tokenize(shakespeareNormalized)
+
+print("\nTest String 3: ", testToken03)
+output = open("ShakespeareTokenized02.txt", "w+")
+output.write("["+ (", ".join(shakespeareToken03)) +"]")
+
+# #NLTK: Word Punctuation Tokenizer
+
+testToken04 = to.WordPunctTokenizer().tokenize(testNormalized)
+
+shakespeareToken04 = to.WordPunctTokenizer().tokenize(shakespeareNormalized)
+
+print("\nTest String 4: ", testToken03)
+output = open("ShakespeareTokenized02.txt", "w+")
+output.write("["+ (", ".join(shakespeareToken03)) +"]")
+
+# #NLTK: Tweet Tokenizer
+
+# print("Test String 5: " + testToken05)
+# output = open("ShakespeareTokenized05.txt", "w+")
+# output.write(shakespeareToken05)
+
+# #NLTK: MWE Tokenizer
+
+# print("Test String 6: " + testToken06)
+# output = open("ShakespeareTokenized06.txt", "w+")
+# output.write(shakespeareToken06)
+
+# #TextBlob Word Tokenizer
+
+# print("Test String 7: " + testToken07)
+# output = open("ShakespeareTokenized07.txt", "w+")
+# output.write(shakespeareToken07)
+
+# #spaCy Tokenizer
+
+# print("Test String 8: " + testToken08)
+# output = open("ShakespeareTokenized08.txt", "w+")
+# output.write(shakespeareToken08)
+
+# #Gensim Word Tokenizer
+
+# print("Test String 9: " + testToken09)
+# output = open("ShakespeareTokenized09.txt", "w+")
+# output.write(shakespeareToken09)
+
+# #Keras Tokenization
+
+# print("Test String 10: " + testToken10)
+# output = open("ShakespeareTokenized10.txt", "w+")
+# output.write(shakespeareToken10)

13833eaf1769293377fc1a4d94b2b4c6d2b1236d
phlc <30514743+phlc@users.noreply.github.com>
2023-03-07 18:05:51 +0000

    one hot encoding

--- ho-04/main.py
+++ ho-04/main.py
@@ -1,22 +1,39 @@
 from sklearn.datasets import fetch_20newsgroups as fetch_data
 from unidecode import unidecode
 import re
-from datetime import datetime
-
 """
 Functions
 """
+
 
 def remove_punctuation(text):
   return re.sub(r"[^\w\s\d]", '', text)
 
 
+def one_hot_encoding(input):
+  tokens = []
+  for word in input.split():
+    if word not in tokens:
+      tokens.append(word)
+  tokens.sort()
+
+  final_encoding = []
+
+  for line in input.split('\n'):
+    line_encoding = []
+    line_dict = dict.fromkeys(tokens, 0)
+    for word in line.split():
+      line_dict[word] += 1
+    for word in tokens:
+      line_encoding.append(line_dict[word])
+    final_encoding.append(line_encoding)
+
+  return final_encoding
+
 
 """
 Data processing
 """
-data = fetch_data().data
-print(data[0])
 data = []
 
 for input in fetch_data().data:
@@ -24,35 +41,30 @@ for input in fetch_data().data:
   input = input.lower()
   input = remove_punctuation(input)
   data.append(input)
-
-print(data[0])
-
 """
 One-hot Encoding
 """
-
-
+data = data[0:20]
+for input in data:
+  with open("20News_01.txt", "w+") as f:
+    input_encoding = one_hot_encoding(input)
+    for line in input_encoding:
+      line = [str(i) for i in line]
+      f.write("[" + ", ".join(line) + "]\n")
+    f.write('\n\n')
+    
 """
 Count Vectors
 """
-
-
-
 """
 TF-IDF
 """
-
-
 """
 N-grams (2-grams)
 """
-
-
 """
 Co-occurrence Vectors (Context Window = 1)
 """
-
-
 """
 Word2Vec
 """

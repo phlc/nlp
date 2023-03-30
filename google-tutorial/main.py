a33308cd171307626937e033cc6520cdb2261858
phlc <pedrocarvalho@outlook.com>
2023-03-30 11:24:25 -0300

    Google tutorial started

---
+++ google-tutorial/main.py
@@ -0,0 +1,51 @@
+import random
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+"""
+Functions
+"""
+
+def load_imdb_sentiment_analysis_dataset(seed=123):
+
+    imdb_data_path = 'ignore_data'
+
+    # Load the training data
+    train_texts = []
+    train_labels = []
+    for category in ['pos', 'neg']:
+        train_path = os.path.join(imdb_data_path, 'train', category)
+        for fname in sorted(os.listdir(train_path)):
+            if fname.endswith('.txt'):
+                with open(os.path.join(train_path, fname)) as f:
+                    train_texts.append(f.read())
+                train_labels.append(0 if category == 'neg' else 1)
+
+    # Load the validation data.
+    test_texts = []
+    test_labels = []
+    for category in ['pos', 'neg']:
+        test_path = os.path.join(imdb_data_path, 'test', category)
+        for fname in sorted(os.listdir(test_path)):
+            if fname.endswith('.txt'):
+                with open(os.path.join(test_path, fname)) as f:
+                    test_texts.append(f.read())
+                test_labels.append(0 if category == 'neg' else 1)
+
+    # Shuffle the training data and labels.
+    random.seed(seed)
+    random.shuffle(train_texts)
+    random.seed(seed)
+    random.shuffle(train_labels)
+
+    return ((train_texts, np.array(train_labels)),
+            (test_texts, np.array(test_labels)))
+
+"""
+Main
+"""
+
+train, test = load_imdb_sentiment_analysis_dataset()
+
+print(train[0][0], train[1][0])
